3a91fa56aa7c0c1eb90b562e657e16c479820c49
fannypc <mars.poc@gmail.com>
2018-09-22 14:44:44 -0500

    se agrega librería fpdf

---
+++ ishikawa.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <form class="" action="respuesta.php" method="get">
+      <div>
+        <label>Objetivo</label>
+        <input type="text" name="objetivo" value="">
+      </div>
+        <label>Soluciones</label><br>
+        <div class="">
+          <input type="text" name="solucion1" value="" placeholder="Solucion 1">
+        </div>
+        <div class="">
+          <input type="text" name="solucion2" value="" placeholder="Solucion 2">
+        </div>
+        <div class="">
+          <input type="text" name="solucion3" value="" placeholder="Solucion 3">
+        </div>
+        <div class="">
+          <input type="text" name="solucion4" value="" placeholder="Solucion 4">
+        </div>
+        <div class="">
+          <input type="text" name="solucion5" value="" placeholder="Solucion 5">
+        </div>
+        <div class="">
+          <input type="text" name="solucion6" value="" placeholder="Solucion 6">
+        </div>
+        <button type="button" name="button">Generar diagrama de Ishikawa</button>
+    </form>
+  </body>
+</html>
